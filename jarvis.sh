490624c1a9465d331b2cba43c040f48178548cfd
Bruno Miguel <brunoalexandremiguel@gmail.com>
2019-11-21 23:09:05 +0000

    changes to jarvis.sh

--- jarvis.sh
+++ jarvis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
+set -x
+
 source "$DIR/config"
 export PKGEXT
 export COMPRESSXZ
@@ -19,57 +21,59 @@ build() {
 
     pushd "$DIR/pkgbuild"
 
-    for f in *; do
-        if [ -d "$f" ]; then
-            echo "Processing $f..."
-            pushd "$f"
-            if [ -f "PKGBUILD" ]; then
-                echo "Found PKGBUILD for $f. Building..."
-                # clean build force overwrite
-                PACMAN=/usr/local/bin/pakku makepkg -c -C -s -f --nosign --noconfirm --needed -r --skippgpcheck --skipint
-                    if [ $? -ne 0 ]
-                    then
-                        echo -e "\n!!! ERROR !!! in $f\n" > "$DIR/captains.log"
-                    fi
+        for f in *; do
+            if [ -d "$f" ]; then
+                echo "Processing $f..."
+                pushd "$f"
+                if [ -f "PKGBUILD" ]; then
+                    echo "Found PKGBUILD for $f. Building..."
+                    # clean build force overwrite
+                    PACMAN=/usr/local/bin/pakku makepkg -c -C -s -f --nosign --noconfirm --needed -r --skippgpcheck --skipint
+                        if [ $? -ne 0 ]
+                        then
+                            echo -e "\n!!! ERROR !!! in $f\n" > "$DIR/captains.log"
+                        fi
+                fi
+                popd
             fi
-            popd
-        fi
-    done
+        done
+    popd
 
 }
 
 refresh() {
     pushd "$DIR/pkgbuild"
 
-    # update all submodules
-    for D in */; do
-        cd $D;
-        echo -e "\e[1m$D\e[0m";
-        git clean -x -d -f;
-        git stash;
-        git pull --progress;
-        echo -e "\n";
-        #sleep .002s;
-        cd ..;
-    done
+        # update all submodules
+        for D in */; do
+            cd $D;
+            echo -e "\e[1m$D\e[0m";
+            git clean -x -d -f;
+            git stash;
+            git pull --progress;
+            echo -e "\n";
+            #sleep .002s;
+            cd ..;
+        done
+    popd
 }
 
 deploy() {
     # move built packages to cache/
     pushd "$DIR/repository"
-    for f in *${PKGEXT}; do
-        [ -f "$f" ] || break
-        echo "Archiving $f..."
-        mv "$f" "$BUILDDIR"
-    done
+        for f in *${PKGEXT}; do
+            [ -f "$f" ] || break
+            echo "Archiving $f..."
+            mv "$f" "$BUILDDIR"
+        done
 
-    # add built packages to repository database
-    for f in ${PKGDEST}/*${PKGEXT}; do
-        [ -f "$f" ] || break
-        echo "Deploying $f..."
-        mv "$f" "./"
-        repo-add -s -v "${REPONAME}.db.tar.gz" "$(basename "$f")"
-    done
+        # add built packages to repository database
+        for f in ${PKGDEST}/*${PKGEXT}; do
+            [ -f "$f" ] || break
+            echo "Deploying $f..."
+            mv "$f" "./"
+            repo-add -s -v "${REPONAME}.db.tar.gz" "$(basename "$f")"
+        done
     popd
 }
 
@@ -78,7 +82,7 @@ sync() {
 }
 
 add() {
-    git submodule add "https://aur.archlinux.org/$1" "./pkgbuild/$1"
+    git submodule add "https://aur.archlinux.org/$2" "./pkgbuild/$2"
 }
 
 delete() {
