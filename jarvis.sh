3ad7b7b201bbfd0c175ab76dc748fd7c279a365a
bruno miguel <brunoalexandremiguel@gmail.com>
2020-04-10 11:44:09 +0000

    fixed issue with submodule removal in jarvis

--- jarvis.sh
+++ jarvis.sh
@@ -112,7 +112,7 @@ delete() {
     git rm --cached "$DIR/pkgbuild/${OPTARG}"
     rm -rf "$DIR/pkgbuild/${OPTARG}"
     git commit -m "Removed ${OPTARG} submodule"
-    rm -rf ".git/modules/$DIR/pkgbuild/${OPTARG}"
+    rm -rf "$DIR/.git/modules/pkgbuild/${OPTARG}"
     git config -f .gitmodules --remove-section "submodule.pkgbuild/${OPTARG}"
     git config -f .git/config --remove-section "submodule.pkgbuild/${OPTARG}"
 }
