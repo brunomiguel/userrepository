a408f01681be5ce9c25883e50af72cd0e93ea42c
Bruno Miguel <brunoalexandremiguel@gmail.com>
2023-01-15 21:47:30 +0000

    new packages; submodules updates

--- roomba.sh
+++ roomba.sh
@@ -13,4 +13,4 @@ cd $HOME
 #
 # TODO
 # Put these paths in a list and iterate through them
-sudo rm go/ .cargo/ .npm/* .yarn/ .nvm/* .cache/* .electron-gyp/ .dotnet/* .nimble/ .flutter .freeplane/* .slime/* .node-gyp/* .subversion/* .stack/* .lein/* .pub-cache/* .dart/ .java/* .pnpm-store/* .nuget/* .m2/* .timetrace/* .vst3/* .ccache/* .nvm/* .cache/ .gradle/ .local/share/godot -rf; sudo pacman -Sccc; pikaur -Sccc; paru -Sccc
+sudo rm go/ .cargo/ .npm/* .yarn/ .nvm/* .cache/* .electron-gyp/ .dotnet/* .nimble/ .flutter .freeplane/* .slime/* .node-gyp/* .subversion/* .stack/* .lein/* .pub-cache/* .dart/ .java/* .pnpm-store/* .nuget/* .m2/* .timetrace/* .vst3/* .ccache/* .nvm/* .cache/ .gradle/ .local/share/godot .local/share/pnpm -rf; sudo pacman -Sccc; pikaur -Sccc; paru -Sccc
