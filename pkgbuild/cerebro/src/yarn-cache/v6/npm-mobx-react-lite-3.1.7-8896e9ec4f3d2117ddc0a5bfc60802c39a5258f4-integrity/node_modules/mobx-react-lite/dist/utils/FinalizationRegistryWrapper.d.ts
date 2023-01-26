f7e6ee80c757235477d575080121bbe1895aa3b0
Bruno Miguel <brunoalexandremiguel@gmail.com>
2023-01-26 21:41:42 +0000

    new packages; submodules updates

--- pkgbuild/cerebro/src/yarn-cache/v6/npm-mobx-react-lite-3.1.7-8896e9ec4f3d2117ddc0a5bfc60802c39a5258f4-integrity/node_modules/mobx-react-lite/dist/utils/FinalizationRegistryWrapper.d.ts
+++
@@ -1,7 +0,0 @@
-declare class FinalizationRegistryType<T> {
-    constructor(cleanup: (cleanupToken: T) => void);
-    register(object: object, cleanupToken: T, unregisterToken?: object): void;
-    unregister(unregisterToken: object): void;
-}
-declare const FinalizationRegistryLocal: typeof FinalizationRegistryType | undefined;
-export { FinalizationRegistryLocal as FinalizationRegistry };
